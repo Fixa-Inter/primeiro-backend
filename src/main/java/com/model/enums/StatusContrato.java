2d2d7a9aa00ac808fcb234ca5ea516d56ab684d5
Miguel Monteiro <miguelantonio17072010@gmail.com>
2026-09-23 21:20:12 -0300

    fix: arrumando codigos do DAO e model de Pagamento, e ajustes no enum MetodoPagamento

--- src/main/java/com/model/enums/StatusContrato.java
+++ src/main/java/com/model/enums/StatusContrato.java
@@ -2,9 +2,9 @@ package com.model.enums;
 
 public enum StatusContrato {
 
-    Ativo(1),
-    Inativo(2),
-    Cancelado(3);
+    ATIVO("Ativo", 1),
+    INATIVO(2),
+    CANCELADO(3);
 
     final int codigo;
 
@@ -16,24 +16,6 @@ public enum StatusContrato {
         return codigo;
     }
 
-    // metodo para converter o codigo para o valor
-    public static StatusContrato getNomeComBaseCodigo(int codigo) {
-        return switch (codigo) {
-            case 1 -> Ativo;
-            case 2 -> Inativo;
-            case 3 -> Cancelado;
-            default -> throw new IllegalArgumentException("Tipo de instituicao invalido: ");
-        };
-    }
 
-    // metodo para converter o valor para o codigo
-    public static int getCodigoComBaseNome(String campo) {
-        return switch (campo) {
-            case Ativo -> 1;
-            case Inativo -> 2;
-            case Cancelado -> 3;
-            default -> throw new IllegalArgumentException("Codigo de instituicao invalido: ");
-        };
-    }
 
 }
